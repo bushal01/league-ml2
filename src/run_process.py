38853980dfa205418753ae89debb7565b354b89f
bushal01 <albertbush@gmail.com>
2019-12-16 03:20:48 -0800

    Started skeleton code of run_process.py

---
+++ src/run_process.py
@@ -0,0 +1,32 @@
+"""Point of entry script for all processes -- crawling, training, scoring."""
+
+def crawl():
+    """Collect already crawled players and matches, and continue crawling.
+    params:
+    - id_to_start_crawl
+    - scanned_players_file
+    - unscanned_players_file
+    - scanned_matches_file
+    - unscanned_matches_file
+    - output_file
+
+    """
+    return
+
+
+def train():
+    """Train a specific model (each model will get its own script to enable ensemble)."""
+    return
+
+
+def score():
+    """Score a set of matches."""
+    return
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
